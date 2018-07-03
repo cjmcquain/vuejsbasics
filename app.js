5caa61d6cb7c0e909baee7ff8208b28323b3410c
Charles McQuain <charlesmcquain@gmail.com>
2018-07-02 22:19:59 -0700

    Added first directive v-bind to link on homepage

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 new Vue({
   el: "#app",
   data: {
-    title: "Becoming a Vue ninja"
+    title: "Becoming a Vue ninja",
+    name: "Charles",
+    url: "http://www.youtube.com"
   },
   methods: {
-    greet() {
-      return `Hello there`;
+    greet(time) {
+      return `Hello there and good ${time}, ${this.name}`;
     }
   }
 });
