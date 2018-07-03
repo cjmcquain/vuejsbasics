e8fcc60cd5414d51756d4327d3646f2a81c58247
Charles McQuain <charlesmcquain@gmail.com>
2018-07-02 22:29:14 -0700

    Added buttons with click events

--- app.js
+++ app.js
@@ -2,13 +2,7 @@ new Vue({
   el: "#app",
   data: {
     title: "Becoming a Vue ninja",
-    name: "Charles",
-    url: "http://www.youtube.com",
-    classes: ["one", "two"]
+    wage: 10
   },
-  methods: {
-    greet(time) {
-      return `Hello there and good ${time}, ${this.name}`;
-    }
-  }
+  methods: {}
 });
